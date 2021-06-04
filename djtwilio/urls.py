835ef6dc15e68e89525237811b4a1734d47b7da7
Devang Sharma <devshu25@gmail.com>
2021-06-05 03:31:25 +0530

    Twilio- Django Application

---
+++ djtwilio/urls.py
@@ -0,0 +1,18 @@
+# Author : devshu25@gmail.com
+
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django1.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    #Here we add our Twilio URLs
+    url(r'^sms/$', 'djtwilio.views.sms'),
+    url(r'^ring/$', 'djtwilio.views.ring'),
+)
